5349226f320ab9b817a85343046a2f363da0fdf2
avoajaugochukwu <karlthegoo@hotmail.com>
2017-01-29 18:13:13 +0100

    update tweet box app

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,57 +1,60 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var $ = require('jquery');
-
-var data = {};
 
 
-$(document).ready(function() {
-
-
-class Layout extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {quoter: "", author: ""};
-	}
-
-
-	render() {
-		return (
-			<div>
-				
-				<button className="btn btn-lg btn-primary" onClick={this.changeBackgroundAndQuote}>Change Color</button>
-			</div>
-		);
-	}
-
-
-	changeBackgroundAndQuote () {
-		var colorArray = ['#82E0AA', '#5DADE2', '#E8DAEF', '#F6DDCC', '#AEB6BF'];
-		var randomNumber = Math.floor(Math.random() * colorArray.length);
-		document.body.style.backgroundColor = colorArray[randomNumber];
-			$.getJSON({
-	        url: "http://quotes.stormconsultancy.co.uk/random.json"
-	  }).then(function(data) {
-	  		$('.greeting-id').empty();
-		    $('.greeting-content').empty();
-
-
-		    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + data.quote + '" ' + data.author));
-		    $('.greeting-id').append(data.quote);
-		    $('.greeting-content').append(data.author);
-		  });
-
-	  console.log(data.quote);
-	}
-
-
-}
-
-
-
-
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
-
-
+var TweetBox = React.createClass({
+  getInitialState: function() {
+    return {
+      text: "",
+      photoAdded: false
+    };
+  },
+  handleChange: function(event) {
+    this.setState({ text: event.target.value });
+  },
+  togglePhoto: function (event) {
+    this.setState({ photoAdded: !this.state.photoAdded });
+  },
+  remainingCharacters: function() {
+    if (this.state.photoAdded) {
+      return 140 - 23 -this.state.text.length;
+    } else {
+        return 140 - this.state.text.length;
+    }
+  },
+  overflowAlert: function () {
+  console.log("I am here");
+    if (this.remainingCharacters() < 0) {
+      var beforeOverflowText = this.state.text.substring(140 -10, 140);
+      var overflowText = this.state.text.substring(140);
+      return (
+        <div className="alert alert-warning">
+          <strong>Oops! Too long:</strong>
+          &nbsp;...{beforeOverflowText}
+          <strong className="bg-danger">{overflowText}</strong>
+        </div>
+      );
+    } else {
+      return "";
+    }
+  },
+  render: function () {
+    return (
+      <div className="well clearfix">
+        {this.overflowAlert()}
+        <textarea className="form-control" onChange={this.handleChange}></textarea>
+        <br/>
+        <span>{this.remainingCharacters()}</span>
+        <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0 !this.state.photoAdded}>Tweet</button>
+        <button className="btn btn-default pull-right"
+          onClick={this.togglePhoto}>
+          {this.state.photoAdded ? "✓ Photo Added" : "Add Photo"}
+        </button>
+      </div>
+    );
+  }
 });
+
+ReactDOM.render(
+  <TweetBox />, document.getElementById("app")
+);
